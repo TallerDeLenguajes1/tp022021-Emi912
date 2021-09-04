618964e44a420790c7722ae7645eb7f7e673e5a1
Emi Merchan <emimerchan83@gmail.com>
2021-09-03 22:51:33 -0300

    Se termino el tp

---
+++ TP02/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TP02.Models;
+
+namespace TP02.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Form(string nombre, string apellido, DateTime fnacimiento, string edad, DateTime fechadeIngreso, string rol, string direccion)
+        {
+            try
+            {
+                int Edad = Int32.Parse(edad);
+                Empleado MiEmpleado = new Empleado(nombre, apellido, Edad, fechadeIngreso, fnacimiento, rol, direccion);
+                _logger.LogInformation("Se logeo el empleado " + MiEmpleado.ToString());
+                return View(MiEmpleado);
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                return View("Error");
+
+            }
+
+            
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TP02/Models/Empleado.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TP02.Models
+{
+    public class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private double salario;
+        private string direccion;
+        private string rol;
+        private DateTime fechadeingreso;
+        private double antiguedad;
+        private DateTime fechadenacimiento;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+
+        public double Salario { get => salario; set => salario = value; }
+        public string Direccion { get => direccion; set => direccion = value; }
+        public string Rol { get => rol; set => rol = value; }
+        public DateTime Fechadeingreso { get => fechadeingreso; set => fechadeingreso = value; }
+        public double Antiguedad { get => antiguedad; set => antiguedad = value; }
+        public DateTime Fechadenacimiento { get => fechadenacimiento; set => fechadenacimiento = value; }
+
+        public double Edad
+        {
+            get
+            {
+                return edad;
+            }
+            set
+            {
+                edad = DateTime.Today.AddTicks(-Fechadenacimiento.Ticks).Year - 1;
+            }
+        }
+
+       
+
+        public Empleado(string nombre, string apellido, int edad, DateTime fechaingreso, DateTime fechanac, string rol, string direccion)
+        {
+            Random aleatorio = new Random();
+            this.Nombre = nombre;
+            this.Apellido = apellido;
+            this.Edad = edad;
+            this.Fechadenacimiento = fechanac;
+            this.Rol = rol;
+            this.Fechadeingreso = fechaingreso;
+            this.Direccion = direccion;
+            this.Antiguedad = DateTime.Today.AddTicks(-Fechadeingreso.Ticks).Year - 1;
+            this.Salario = CalcularSalario(this.Antiguedad);
+  
+           
+        }
+
+        private double CalcularSalario(double antiguedad)
+        {
+            int SueldoBasico = 15000;
+            return SueldoBasico + (SueldoBasico * 0.1) * antiguedad - SueldoBasico * 0.15;
+        }
+    }
+}
